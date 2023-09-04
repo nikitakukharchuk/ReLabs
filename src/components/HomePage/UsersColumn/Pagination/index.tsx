031c4888d1cde256fdbecba2f004dea85f48f70b
nikitakukharchuk <nikitakukharchuk@mail.ru>
2023-09-04 13:47:48 +0300

    fix_pagination

--- src/components/HomePage/UsersColumn/Pagination/index.tsx
+++ src/components/HomePage/UsersColumn/Pagination/index.tsx
@@ -35,12 +35,14 @@ const PaginationItem = ({
     }
   };
 
+  const totalPages = total && total * 5
   return (
     <div className="flex justify-center pt-5">
       <Pagination
-        current={current}
-        total={total}
-        onChange={onChangeUsersList}
+          onChange={onChangeUsersList}
+          total={totalPages}
+          current={current}
+          pageSize={5}
       />
     </div>
   );
